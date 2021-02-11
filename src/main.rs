34910e8c0d4bfa3aad8466dd417ca214aba2cad5
Oscar Franzen <p.oscar.franzen@gmail.com>
2021-02-11 11:16:29 +0100

    new topics are marked green

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,9 @@
 
 use std::io::prelude::*;
 use std::io::Write;
+use std::fs::File;
+use std::path::Path;
 use std::net::TcpStream;
-use std::sync::Arc;
 use std::env;
 use std::process;
 use std::collections::HashMap;
@@ -24,18 +25,22 @@ use rustls;
 use webpki;
 use regex::Regex;
 use getopts::Options;
+use home;
 
 const MESSAGE_SIZE : usize  = 1024;
 const HOSTNAME: &str = "www.svt.se";
+const VERSION: &str = "0.1";
+const CACHE_FILE: &str = "topics";
 
-struct texttv {
+struct Texttv {
     useragent : String,
+    conf_dir : String,
     stream : Option<rustls::StreamOwned<rustls::ClientSession, TcpStream>>
 }
 
-impl texttv {
+impl Texttv {
     fn http_connect(&mut self, hostname : &str) { //-> rustls::StreamOwned<rustls::ClientSession, TcpStream> {
-	let mut socket = TcpStream::connect(hostname.to_owned() + ":443").unwrap();
+	let socket = TcpStream::connect(hostname.to_owned() + ":443").unwrap();
 	let mut config = rustls::ClientConfig::new();
 
 	config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
@@ -70,7 +75,10 @@ impl texttv {
 
     fn http_get(&mut self, path : u32) {
 	let s = self.http_format(path);
-	self.stream.as_mut().unwrap().write(s.as_bytes());
+	match self.stream.as_mut().unwrap().write(s.as_bytes()) {
+	    Err(e) => println!("{:?}", e),
+	    _ => ()
+	}
     }
 
     fn http_recv(&mut self) -> String {
@@ -90,13 +98,12 @@ impl texttv {
 
 	let str = &std::str::from_utf8(&msg).unwrap();
 
-	//let mut file = std::fs::File::create("data.txt").expect("create failed");
-	//file.write_all(str.as_bytes()).expect("write failed");
-
 	str.to_string()
     }
 
-    fn parse_topics(&mut self, contents : &str) {
+    fn parse_topics(&mut self, contents : &str,
+		    save : &mut Vec<String>,
+		    old : &HashMap<String, u32>) {
 	// remove the header
 	let i = contents.find("\r\n\r\n").unwrap() + 4;
 	let contents = contents.get(i..).unwrap();
@@ -122,7 +129,15 @@ impl texttv {
 		    let title = re_html.replace_all(title, "");
 		    let title = title.trim_start();
 
-		    println!("{} [{}]", title, index);
+		    let pr = (*old).get(title);
+
+		    save.push((&title).to_string());
+
+		    if pr == None {
+			println!("\u{1b}[92m{} [{}]\u{1b}[0m", title, index);
+		    } else {
+			println!("{} [{}]", title, index);
+		    }
 		}
 	    }
 	}
@@ -155,26 +170,69 @@ impl texttv {
 	}
     }
 
+    fn write_cache(&mut self, save : &mut Vec<String>) {
+	let mut file = File::create(format!("{}/{}",
+					    self.conf_dir,
+					    CACHE_FILE)).unwrap();
+	
+	for topic in save {
+	    match file.write_all(topic.as_bytes()) {
+		Err(e) => println!("{:?}", e),
+		_ => ()
+	    }
+	    
+	    match file.write_all("\n".as_bytes()) {
+		Err(e) => println!("{:?}", e),
+		_ => ()
+	    }
+	}
+    }
+
+    fn load_cache(&mut self, old : &mut HashMap<String, u32>) {
+	let c = std::fs::read_to_string(format!("{}/{}",
+						self.conf_dir,
+						CACHE_FILE)).unwrap();
+
+	for q in c.split("\n") {
+	    old.insert(q.to_string(), 1);
+	}
+    }
+
     fn show_menu(&mut self) {
 	let pages = [101, 102, 103, 104, 105];
+	let mut topics = Vec::new();
+	let mut old = HashMap::new();
+
+	if Path::new(&format!("{}/{}", self.conf_dir, CACHE_FILE)).exists() {
+	    self.load_cache(&mut old);
+	}
+	
 	for page in &pages {
 	    self.http_get(*page);
 	    let d = self.http_recv();
-	    self.parse_topics(&d);
+	    self.parse_topics(&d, &mut topics, &old);
 	}
+
+	self.write_cache(&mut topics);
     }
 
     fn help(&mut self) {
 	println!("This is an extremely simple tele text client for Swedish Text TV.");
 	println!("Project lives at https://github.com/oscar-franzen/text-tv-client");
+	println!("\nversion: {}\n", VERSION);
 	println!("Feedback to: <p.oscar.franzen@gmail.com>");
     }
 
     fn init(&mut self) {
 	let s = format!("{}{}",
-			env::home_dir().unwrap().display(),
+			home::home_dir().unwrap().display(),
 			"/.text_tv_client");
-	std::fs::create_dir(s);
+	match std::fs::create_dir(&s) {
+	    Err(e) => (),
+	    _ => ()
+	}
+	
+	self.conf_dir = s;
     }
 }
 
@@ -189,44 +247,46 @@ fn usage() {
 
 fn main() {
     let args : Vec<String> = env::args().collect();
-    let mut useragent = String::new();
-
     let mut opts = Options::new();
+
     opts.optopt("u", "useragent", "", "USERAGENT");
     opts.optflag("h", "help", "");
+    opts.optflag("v", "version", "");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
     };
-
+    
+    let mut tt = Texttv { conf_dir : "".to_string(),
+			  useragent: "".to_string(),
+			  stream: None };
+    
     if matches.opt_present("u") {
-        useragent = matches.opt_str("u").unwrap();
+        tt.useragent = matches.opt_str("u").unwrap();
     } else {
-	useragent = "Text-tv-client, \
+	tt.useragent = "Text-tv-client, \
 		     https://github.com/oscar-franzen/text-tv-client".to_string();
     }
-
-    let mut tt = texttv { useragent: useragent,
-			  stream: None };
 
     if matches.opt_present("h") {
 	usage();
     }
 
+    if matches.opt_present("v") {
+	tt.help();
+	process::exit(0);
+    }
+
+
     tt.init();
     tt.http_connect(HOSTNAME);
     tt.show_menu();
 
-    // // let mut fh = std::fs::File::open("data.txt").unwrap();
-    // // let mut d = String::new();
-    // // fh.read_to_string(&mut d).unwrap();
-    // //process::exit(0);
-
     loop {
 	println!("\nGo to page [NUMBER, m for menu, x to exit, h for help]: ");
 	let mut inp_page = String::new();
-	let b1 = std::io::stdin().read_line(&mut inp_page).unwrap();
+	std::io::stdin().read_line(&mut inp_page).unwrap();
 	let inp_page = inp_page.trim_end();
 
 	if inp_page == "x" {
